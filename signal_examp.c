0eb8707a2ff5557a68569a3f642a633cc820459b
cj <zhangyafeng.cj@gmail.com>
2015-05-20 17:59:36 +0800

    信号处理

--- signal_examp.c
+++ signal_examp.c
@@ -1,18 +1,22 @@
 #include <signal.h>
 #include <stdio.h>
 
+extern void psignal (int __sig, const char *__s);
+extern __sighandler_t sigset (int __sig, __sighandler_t __disp) __THROW;
+extern int sigignore (int __sig) __THROW;
+
 
 void sig_usr(int signo)
 {
-        if (signo == SIGINT)
-                puts("\nGet ctrl-c, quting");
-
+        if (signo == SIGINT) {
+                puts("still running (at sig_usr)");
+                signal(SIGINT, SIG_IGN);
+        }
 }
 
 int main(int argc, char *argv[])
 {
-        if (signal(SIGINT, sig_usr) == SIG_ERR)
-                puts("signal get error");
+
         // void (*signal(int, void (*)(int)))(int);
         /*
           SIG_IGN
@@ -21,7 +25,14 @@ int main(int argc, char *argv[])
         */
 
         int tmp;
-        scanf("%d", &tmp);
-        printf("tmp: %d\n", tmp);
+        while (1){
+                if (SIG_ERR == signal(SIGINT, sig_usr)) {
+                        puts("SIGINT error");
+                        break;
+                }
+                scanf("%d", &tmp);
+                printf("tmp: %d\n", tmp);
+                if (!tmp) break;
+        }
         return 0;
 }
