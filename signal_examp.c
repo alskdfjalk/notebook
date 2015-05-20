c98e271e377b68a48db130454b6ebb6e4e36efe7
cj <zhangyafeng.cj@gmail.com>
2015-05-20 18:00:54 +0800

    信号处理

--- signal_examp.c
+++ signal_examp.c
@@ -1,11 +1,6 @@
 #include <signal.h>
 #include <stdio.h>
 
-extern void psignal (int __sig, const char *__s);
-extern __sighandler_t sigset (int __sig, __sighandler_t __disp) __THROW;
-extern int sigignore (int __sig) __THROW;
-
-
 void sig_usr(int signo)
 {
         if (signo == SIGINT) {
